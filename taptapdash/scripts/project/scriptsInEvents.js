e5b6eb40fadd0a358c945e03a4dcf1beb7548c55
arturbier <78679486+arturbier@users.noreply.github.com>
2021-04-16 13:45:03 +0600

    Add files via upload

---
+++ taptapdash/scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Main_Event91_Act2(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7816086, "location": "https://vk.com/superkiwiadventure"});
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
