7d52bb1b39d14a59f7e5e8acab7fff6ba139fa38
arturbier <78679486+arturbier@users.noreply.github.com>
2021-04-16 17:56:46 +0600

    Add files via upload

--- taptapdash/scripts/project/scriptsInEvents.js
+++ taptapdash/scripts/project/scriptsInEvents.js
@@ -10,19 +10,26 @@
 			vkBridge.send("VKWebAppOpenApp", {"app_id": 7816086, "location": "https://vk.com/superkiwiadventure"});
 		},
 
-		async Menubutton_Event3_Act1(runtime, localVars)
+		async Menubutton_Event2_Act3(runtime, localVars)
 		{
-			vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"});
+			vkbridge.send("VKWebAppTapticNotificationOccurred", {"type": "error"}); //error, success, warning
+		},
+
+		async Menubutton_Event3_Act2(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"}); //light, medium, heavy
 		},
 
 		async Menubutton_Event4_Act1(runtime, localVars)
 		{
-			vkbridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"});
+			vkBridge.send("VKWebAppShowNativeAds", {ad_format:"preloader"}) 
+			.then(data => console.log(data.result)) 
+			.catch(error => console.log(error));
 		},
 
-		async Menubutton_Event4_Act2(runtime, localVars)
+		async Menubutton_Event5_Act2(runtime, localVars)
 		{
-			vkbridge.send("VKWebAppShowNativeAds", {ad_format:"reward"});
+			vkbridge.send("VKWebAppTapticSelectionChanged", {}); 
 		}
 
 	};
