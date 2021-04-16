6e44b8bef5cf4288f821bf4302b3564433a95028
arturbier <78679486+arturbier@users.noreply.github.com>
2021-04-16 18:28:14 +0600

    Add files via upload

---
+++ kiwinew/scripts/project/scriptsInEvents.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Etittlescreen_Event40_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppCheckNativeAds", {ad_format:"reward"});
+		},
+
+		async Etittlescreen_Event40_Act3(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppShowNativeAds", {ad_format:"reward"});
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
