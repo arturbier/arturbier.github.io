8ee6e906c3983f38b3679d5e195430660afaed5f
arturbier <78679486+arturbier@users.noreply.github.com>
2021-04-18 03:22:23 +0600

    Add files via upload

---
+++ fallordeadnew/scripts/project/scriptsInEvents.js
@@ -0,0 +1,41 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Main_s_Event25_Act2(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"}); //light, medium, heavy
+		},
+
+		async Main_s_Event28_Act2(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"}); //light, medium, heavy
+		},
+
+		async Main_s_Event31_Act2(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"}); //light, medium, heavy
+		},
+
+		async Menu_s_Event17_Act2(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"}); //light, medium, heavy
+		},
+
+		async Menu_s_Event57_Act2(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"}); //light, medium, heavy
+		},
+
+		async Menu_s_Event63_Act2(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"}); //light, medium, heavy
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
