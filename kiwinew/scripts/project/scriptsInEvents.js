6813abb1bc79f5fc4c2c916f31816047d576f50e
arturbier <78679486+arturbier@users.noreply.github.com>
2021-04-25 15:44:30 +0600

    Add files via upload

--- kiwinew/scripts/project/scriptsInEvents.js
+++ kiwinew/scripts/project/scriptsInEvents.js
@@ -5,9 +5,29 @@
 {
 	const scriptsInEvents = {
 
-		async Etittlescreen_Event40_Act1(runtime, localVars)
+		async Etittlescreen_Event66_Act1(runtime, localVars)
 		{
-			vkBridge.send("VKWebAppCheckNativeAds", {ad_format:"reward"});
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7816086, "location": "https://vk.com/superkiwiadventure"});
+		},
+
+		async Etittlescreen_Event67_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7818856, "location": "https://vk.com/fallordead"});
+		},
+
+		async Etittlescreen_Event68_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7824963, "location": "https://vk.com/taptapjump"});
+		},
+
+		async Etittlescreen_Event69_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7815639, "location": "https://vk.com/squarecats"});
+		},
+
+		async Etittlescreen_Event70_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7833257, "location": "https://vk.com/app7833257"});
 		}
 
 	};
