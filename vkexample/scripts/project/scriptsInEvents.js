a78e2afdcb9c8be3e143f0a5120882f7b7099339
arturbier <78679486+arturbier@users.noreply.github.com>
2021-04-26 15:06:51 +0600

    Add files via upload

--- vkexample/scripts/project/scriptsInEvents.js
+++ vkexample/scripts/project/scriptsInEvents.js
@@ -1,34 +1,100 @@
 "use strict";
 
-function addJavaScript(src, type) {
+function VKPlatformProvider(options) {
+    this.options = { ... options }
+	this.userId = null
+	
+    if (!this.options.minDelayBetweenAds)
+        this.options.minDelayBetweenAds = 30
+}
+
+VKPlatformProvider.prototype.initialize = function() {
     return new Promise(resolve => {
-        let script = document.createElement('script')
-        script.src = src
+        addJavaScript('https://unpkg.com/@vkontakte/vk-bridge/dist/browser.min.js')
+            .then(() => {
+                vkBridge.send('VKWebAppInit')
+                    .then(() => {
+                        if (this.options.logsEnabled)
+                            console.log('VKPlatformProvider: initialized')
 
-        if (type)
-            script.type = type
-
-        script.addEventListener('load', function() {
-            resolve()
-        })
-
-        document.head.appendChild(script)
+                        resolve()
+                    })
+            })
     })
 }
 
-function Timer(seconds) {
-    let self = this
-    self.seconds = seconds
-    self.isCompleted = false
-    self._timer = setInterval(() => {
-        self.seconds -= 1
+VKPlatformProvider.prototype.getUserId = function() {
+    return new Promise(resolve => {
+        if (this.userId != null) {
+            resolve(this.userId)
 
-        if (self.seconds <= 0) {
-            self.seconds = 0
-            self.isCompleted = true
-            clearInterval(self._timer)
+            if (this.options.logsEnabled)
+                console.log('VKPlatformProvider.getUserId, result: ' + this.userId)
+
+            return
         }
-    }, 1000)
+
+        this.userId = new URL(window.location.href).searchParams.get('viewer_id')
+
+        if (this.options.logsEnabled)
+            console.log('VKPlatformProvider.getUserId, result: ' + this.userId)
+
+        resolve(this.userId)
+    })
+}
+
+VKPlatformProvider.prototype.showInterstitial = function(ignoreDelay = false) {
+    if (!ignoreDelay && this._nextAdsAvailableTimer != null && !this._nextAdsAvailableTimer.isCompleted)
+        return
+
+    if (this.options.logsEnabled)
+        console.log('VKPlatformProvider.showInterstitial')
+
+    vkBridge.send('VKWebAppShowNativeAds', { ad_format: 'preloader' }).catch(error => console.log(error))
+
+    if (!ignoreDelay)
+        this._nextAdsAvailableTimer = new Timer(this.options.minDelayBetweenAds)
+}
+
+VKPlatformProvider.prototype.showRewarded = function() {
+    if (this.options.logsEnabled)
+        console.log('VKPlatformProvider.showRewarded')
+
+    vkBridge.send('VKWebAppShowNativeAds', { ad_format: 'reward' }).catch(error => console.log(error))
+}
+
+VKPlatformProvider.prototype.vibrate = function() {
+    if (this.options.logsEnabled)
+        console.log('VKPlatformProvider.vibrate')
+
+    // TODO: Add other types
+    let type = 'warning'
+
+    vkBridge.send('VKWebAppTapticNotificationOccurred', { 'type': type })
+}
+
+VKPlatformProvider.prototype.share = function(options) {
+    if (this.options.logsEnabled)
+        console.log('VKPlatformProvider.share, options: ', options)
+
+    if (!options)
+        vkBridge.send('VKWebAppShare')
+    else
+        vkBridge.send('VKWebAppShowWallPostBox', { 'message': options.message, 'attachments': options.link })
+}
+
+VKPlatformProvider.prototype.addToFavorites = function() {
+    if (this.options.logsEnabled)
+        console.log('VKPlatformProvider.addToFavorites')
+
+    vkBridge.send('VKWebAppAddToFavorites')
+}
+
+VKPlatformProvider.prototype.showLeaderboard = function(userResult) {
+    if (this.options.logsEnabled)
+        console.log('VKPlatformProvider.showLeaderboard, userResult: ' + userResult)
+
+    vkBridge.send('VKWebAppShowLeaderBoardBox', { 'user_result': userResult })
 }
 
 {
