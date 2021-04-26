22bfd1a40a78c45193dc91e5259e01b6e9a9b1c3
arturbier <78679486+arturbier@users.noreply.github.com>
2021-04-26 15:44:43 +0600

    Add files via upload

---
+++ vkbridgec3/scripts/project/main.js
@@ -0,0 +1,21 @@
+import VKPlatformProvider from './VKPlatformProvider.js'
+
+let gameRuntime = null
+
+runOnStartup(async runtime =>
+{
+	gameRuntime = runtime
+    runtime.addEventListener('afterprojectstart', () => OnAfterProjectStart())
+})
+
+async function OnAfterProjectStart()
+{
+	const PLATFORM_OPTIONS = {
+		logsEnabled: true
+	}
+	
+	window.platformProvider = new VKPlatformProvider(PLATFORM_OPTIONS)
+	platformProvider.initialize().then(() => {
+		gameRuntime.callFunction('onPlatformProviderInitialized')
+	})
+}
---
+++ vkbridgec3/scripts/project/scriptsInEvents.js
@@ -0,0 +1,50 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Maineventsheet_Event3_Act1(runtime, localVars)
+		{
+			platformProvider.showInterstitial()
+		},
+
+		async Maineventsheet_Event4_Act1(runtime, localVars)
+		{
+			platformProvider.showRewarded()
+		},
+
+		async Maineventsheet_Event5_Act1(runtime, localVars)
+		{
+			platformProvider.vibrate()
+		},
+
+		async Maineventsheet_Event6_Act2(runtime, localVars)
+		{
+			platformProvider.share()
+		},
+
+		async Maineventsheet_Event7_Act2(runtime, localVars)
+		{
+			let options = {
+				message: 'Example message',
+				link: 'https://vk.com/a_b_vstudio'
+			}
+			platformProvider.share(options)
+		},
+
+		async Maineventsheet_Event8_Act1(runtime, localVars)
+		{
+			platformProvider.addToFavorites()
+		},
+
+		async Maineventsheet_Event9_Act1(runtime, localVars)
+		{
+			platformProvider.showLeaderboard(5)
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
