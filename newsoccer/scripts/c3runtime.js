86a53d87a73e833903f2c774e614ef2d2898be6d
arturbier <78679486+arturbier@users.noreply.github.com>
2021-05-05 00:48:50 +0600

    Add files via upload

--- newsoccer/scripts/c3runtime.js
+++ newsoccer/scripts/c3runtime.js
@@ -9602,8 +9602,6 @@ this._stage=0;this._stageTimeLeft+=this._onTime}this._runtime.UpdateRender()}}Ge
 		C3.Plugins.filechooser.Cnds.OnChanged,
 		C3.Plugins.Browser.Acts.ExecJs,
 		C3.Plugins.filechooser.Exps.FileURLAt,
-		C3.Plugins.VKBridge.Cnds.UserGetSuccess,
-		C3.Plugins.VKBridge.Exps.UserData,
 		C3.Plugins.Sprite.Acts.LoadURL,
 		C3.Plugins.System.Acts.SetLayerVisible,
 		C3.Plugins.Sprite.Cnds.IsOverlapping,
@@ -10419,10 +10417,6 @@ this._stage=0;this._stageTimeLeft+=this._onTime}this._runtime.UpdateRender()}}Ge
 		p => {
 			const n0 = p._GetNode(0);
 			return () => (("var img = new Image(); \nimg.crossOrigin = 'Anonymous'; \nimg.onload = function() { \nvar _c = document.createElement('canvas'); \nvar _ctx = _c.getContext('2d'); \n_c.width = 178; _c.height =178;\nvar cur_size_ =img.height;\nif(img.width< img.height) {var cur_size_ = img.width;   }\n_ctx.drawImage(img, 0, 0, cur_size_, cur_size_, 0, 0, 179, 179); \nc2_callFunction('get_data',[_c.toDataURL()]) \n} \nimg.src = '" + n0.ExpObject(0)) + "'");
-		},
-		p => {
-			const f0 = p._GetNode(0).GetBoundMethod();
-			return () => (("var img = new Image(); \nimg.crossOrigin = 'Anonymous'; \nimg.onload = function() { \nvar _c = document.createElement('canvas'); \nvar _ctx = _c.getContext('2d'); \n_c.width = 178; _c.height =178;\nvar cur_size_ =img.height;\nif(img.width< img.height) {var cur_size_ = img.width;   }\n_ctx.drawImage(img, 0, 0, cur_size_, cur_size_, 0, 0, 179, 179); \nc2_callFunction('get_data',[_c.toDataURL()]) \n} \nimg.src = '" + f0("photo_200")) + "'");
 		},
 		() => 178,
 		p => {
