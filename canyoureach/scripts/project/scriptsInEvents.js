690043d590a9eefd703fa2e0e0d6eb37a73f8f05
arturbier <78679486+arturbier@users.noreply.github.com>
2021-05-15 20:14:32 +0600

    Add files via upload

--- canyoureach/scripts/project/scriptsInEvents.js
+++ canyoureach/scripts/project/scriptsInEvents.js
@@ -13,6 +13,51 @@
 		async Ev_game_Event34_Act1(runtime, localVars)
 		{
 			window.open('https://vk.com/public204017056')
+		},
+
+		async Ev_game_Event96_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7816086, "location": "https://vk.com/superkiwiadventure"});
+		},
+
+		async Ev_game_Event97_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7818856, "location": "https://vk.com/fallordead"});
+		},
+
+		async Ev_game_Event98_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7824963, "location": "https://vk.com/taptapjump"});
+		},
+
+		async Ev_game_Event99_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7815639, "location": "https://vk.com/squarecats"});
+		},
+
+		async Ev_game_Event100_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7833257, "location": "https://vk.com/app7833257"});
+		},
+
+		async Ev_game_Event101_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7852478, "location": "https://vk.com/clumsybird"});
+		},
+
+		async Ev_game_Event102_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7844501, "location": "https://vk.com/onlinesoccer21"});
+		},
+
+		async Ev_game_Event103_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7847549, "location": "https://vk.com/zombielenny"});
+		},
+
+		async Ev_game_Event104_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7838558, "location": "https://vk.com/canyoureach"});
 		}
 
 	};
