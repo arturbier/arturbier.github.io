5d5a0d957179f123627fb9d1ab78b92c18bef06d
arturbier <78679486+arturbier@users.noreply.github.com>
2021-05-15 20:26:15 +0600

    Add files via upload

--- kiwinew/scripts/project/scriptsInEvents.js
+++ kiwinew/scripts/project/scriptsInEvents.js
@@ -5,29 +5,49 @@
 {
 	const scriptsInEvents = {
 
-		async Etittlescreen_Event80_Act1(runtime, localVars)
+		async Etittlescreen_Event82_Act1(runtime, localVars)
 		{
 			vkBridge.send("VKWebAppOpenApp", {"app_id": 7816086, "location": "https://vk.com/superkiwiadventure"});
 		},
 
-		async Etittlescreen_Event81_Act1(runtime, localVars)
+		async Etittlescreen_Event83_Act1(runtime, localVars)
 		{
 			vkBridge.send("VKWebAppOpenApp", {"app_id": 7818856, "location": "https://vk.com/fallordead"});
 		},
 
-		async Etittlescreen_Event82_Act1(runtime, localVars)
+		async Etittlescreen_Event84_Act1(runtime, localVars)
 		{
 			vkBridge.send("VKWebAppOpenApp", {"app_id": 7824963, "location": "https://vk.com/taptapjump"});
 		},
 
-		async Etittlescreen_Event83_Act1(runtime, localVars)
+		async Etittlescreen_Event85_Act1(runtime, localVars)
 		{
 			vkBridge.send("VKWebAppOpenApp", {"app_id": 7815639, "location": "https://vk.com/squarecats"});
 		},
 
-		async Etittlescreen_Event84_Act1(runtime, localVars)
+		async Etittlescreen_Event86_Act1(runtime, localVars)
 		{
 			vkBridge.send("VKWebAppOpenApp", {"app_id": 7833257, "location": "https://vk.com/app7833257"});
+		},
+
+		async Etittlescreen_Event87_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7852478, "location": "https://vk.com/clumsybird"});
+		},
+
+		async Etittlescreen_Event88_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7844501, "location": "https://vk.com/onlinesoccer21"});
+		},
+
+		async Etittlescreen_Event89_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7847549, "location": "https://vk.com/zombielenny"});
+		},
+
+		async Etittlescreen_Event90_Act1(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppOpenApp", {"app_id": 7838558, "location": "https://vk.com/canyoureach"});
 		}
 
 	};
