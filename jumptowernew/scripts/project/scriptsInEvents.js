1e31c172acf2821bf4288305d060a2a74a271d4e
arturbier <78679486+arturbier@users.noreply.github.com>
2021-05-29 02:34:20 +0600

    Add files via upload

---
+++ jumptowernew/scripts/project/scriptsInEvents.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Buttons_Event9_Act1(runtime, localVars)
+		{
+			vkbridge.send("VKWebAppJoinGroup", {"204017056": 1});
+		},
+
+		async Buttons_Event10_Act1(runtime, localVars)
+		{
+			window.open('https://vk.com/a_b_vstudio')
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
