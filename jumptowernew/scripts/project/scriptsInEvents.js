aa19a3abc60a0f504a5707d18d3ff8f4a3e8dcab
arturbier <78679486+arturbier@users.noreply.github.com>
2021-05-29 18:34:38 +0600

    Add files via upload

--- jumptowernew/scripts/project/scriptsInEvents.js
+++ jumptowernew/scripts/project/scriptsInEvents.js
@@ -5,7 +5,7 @@
 {
 	const scriptsInEvents = {
 
-		async Buttons_Event10_Act1(runtime, localVars)
+		async Buttons_Event12_Act1(runtime, localVars)
 		{
 			window.open('https://vk.com/a_b_vstudio')
 		}
