d747ec018c807c3be2f002999785e4fec9a08781
arturbier <78679486+arturbier@users.noreply.github.com>
2021-06-06 15:43:44 +0600

    Add files via upload

--- jumptowernew/scripts/project/scriptsInEvents.js
+++ jumptowernew/scripts/project/scriptsInEvents.js
@@ -5,6 +5,11 @@
 {
 	const scriptsInEvents = {
 
+		async S_main_Event22_Act2(runtime, localVars)
+		{
+			VK.callMethod('showAllowMessagesFromCommunityBox', 204017056);
+		},
+
 		async Buttons_Event12_Act1(runtime, localVars)
 		{
 			window.open('https://vk.com/a_b_vstudio')
