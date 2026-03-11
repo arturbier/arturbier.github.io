0dfe46d73f9b58efcd49c733980ddd77e1bf0447
arturbier <78679486+arturbier@users.noreply.github.com>
2026-03-11 13:02:18 +0600

    -

---
+++ puzzlegame/game_html/scripts/plugins/Eponesh_GameScore/c3runtime/plugin.js
@@ -0,0 +1,1 @@
+globalThis.C3.Plugins.Eponesh_GameScore=class extends globalThis.ISDKPluginBase{constructor(e){super(e,"Eponesh_GameScore")}Release(){super.Release()}};
---
+++ puzzlegame/game_html/scripts/plugins/Eponesh_GameScore/c3runtime/type.js
@@ -0,0 +1,1 @@
+globalThis.C3.Plugins.Eponesh_GameScore.Type=class extends globalThis.ISDKObjectTypeBase{constructor(e){super(e)}Release(){super.Release()}OnCreate(){}};
