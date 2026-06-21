a97085e544a6069f8200eda86cbbbf0b9b718508
arturbier <78679486+arturbier@users.noreply.github.com>
2026-06-21 17:44:12 +0600

    -

--- playgama_test/scripts/c3main.js
+++
@@ -1,8 +0,0 @@
-import "./c3runtime.js";
-import "./plugins/Sparsha_FirebaseSDK/c3runtime/main.js";
-import "./plugins/Sparsha_FirebaseRealtimeDatabase/c3runtime/main.js";
-import "./plugins/Sparsha_FirebaseAuth/c3runtime/main.js";
-import "./plugins/PlaygamaBridge/c3runtime/main.js";
-import "./plugins/Sparsha_FirebaseAuthExtended/c3runtime/main.js";
-import "./objRefTable.js";
-import "./project/javaScriptInEvents.js";
