5d0de75480ed66aff2f8d1ef33a9d3c9c3a2c745
arturbier <78679486+arturbier@users.noreply.github.com>
2026-07-09 16:27:11 +0600

    -

--- universalSDK/scripts/c3main.js
+++
@@ -1,5 +0,0 @@
-import "./c3runtime.js";
-import "./plugins/EMI_INDO_sweetalert2/c3runtime/main.js";
-import "./objRefTable.js";
-import "./project/universalSDK.js";
-import "./project/javaScriptInEvents.js";
