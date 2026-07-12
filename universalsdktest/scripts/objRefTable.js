39020694de2c5519f37e6d80ed4fde60940128a1
arturbier <78679486+arturbier@users.noreply.github.com>
2026-07-12 22:31:03 +0600

    -

--- universalsdktest/scripts/objRefTable.js
+++ universalsdktest/scripts/objRefTable.js
@@ -4,6 +4,7 @@ self.C3_GetObjectRefTable = function () {
 		C3.Plugins.Button,
 		C3.Plugins.Text,
 		C3.Plugins.Dictionary,
+		C3.Plugins.TextBox,
 		C3.Plugins.System.Cnds.OnLayoutStart,
 		C3.JavaScriptInEvents.списоксобытий1_Event1_Act2,
 		C3.Plugins.System.Cnds.IsGroupActive,
@@ -11,27 +12,28 @@ self.C3_GetObjectRefTable = function () {
 		C3.JavaScriptInEvents.списоксобытий1_Event4_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event5_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event6_Act1,
-		C3.JavaScriptInEvents.списоксобытий1_Event7_Act1,
+		C3.Plugins.System.Acts.AddVar,
+		C3.Plugins.TextBox.Acts.AppendText,
 		C3.JavaScriptInEvents.списоксобытий1_Event8_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event9_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event12_Act1,
-		C3.Plugins.Text.Cnds.HasTags,
-		C3.Plugins.Text.Acts.SetText,
-		C3.Plugins.System.Exps.projectversion,
+		C3.Plugins.System.Acts.Wait,
 		C3.Plugins.Button.Cnds.OnClicked,
 		C3.Plugins.Button.Cnds.HasTags,
+		C3.JavaScriptInEvents.списоксобытий1_Event28_Act1,
+		C3.JavaScriptInEvents.списоксобытий1_Event29_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event30_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event31_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event32_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event33_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event34_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event35_Act1,
-		C3.JavaScriptInEvents.списоксобытий1_Event36_Act1,
-		C3.JavaScriptInEvents.списоксобытий1_Event37_Act1,
+		C3.JavaScriptInEvents.списоксобытий1_Event37_Act2,
 		C3.JavaScriptInEvents.списоксобытий1_Event39_Act2,
-		C3.JavaScriptInEvents.списоксобытий1_Event41_Act2,
-		C3.JavaScriptInEvents.списоксобытий1_Event41_Act3,
-		C3.JavaScriptInEvents.списоксобытий1_Event41_Act5,
+		C3.JavaScriptInEvents.списоксобытий1_Event39_Act3,
+		C3.JavaScriptInEvents.списоксобытий1_Event39_Act5,
+		C3.JavaScriptInEvents.списоксобытий1_Event40_Act1,
+		C3.JavaScriptInEvents.списоксобытий1_Event41_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event42_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event43_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event44_Act1,
@@ -41,16 +43,14 @@ self.C3_GetObjectRefTable = function () {
 		C3.JavaScriptInEvents.списоксобытий1_Event48_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event49_Act1,
 		C3.JavaScriptInEvents.списоксобытий1_Event50_Act1,
-		C3.JavaScriptInEvents.списоксобытий1_Event51_Act1,
-		C3.JavaScriptInEvents.списоксобытий1_Event52_Act1,
-		C3.JavaScriptInEvents.списоксобытий1_Event53_Act1
+		C3.JavaScriptInEvents.списоксобытий1_Event51_Act1
 	];
 };
 self.C3_JsPropNameTable = [
 	{Button: 0},
-	{debug: 0},
 	{TitleText: 0},
 	{World: 0},
+	{debugArea: 0},
 	{coins: 0},
 	{level: 0},
 	{playerName: 0}
@@ -58,7 +58,7 @@ self.C3_JsPropNameTable = [
 
 self.InstanceType = {
 	Button: class extends self.IButtonInstance {},
-	debug: class extends self.ITextInstance {},
 	TitleText: class extends self.ITextInstance {},
-	World: class extends self.IDictionaryInstance {}
+	World: class extends self.IDictionaryInstance {},
+	debugArea: class extends self.ITextInputInstance {}
 }
--- universalsdktest/scripts/project/javaScriptInEvents.js
+++ universalsdktest/scripts/project/javaScriptInEvents.js
@@ -27,11 +27,6 @@ const scriptsInEvents = {
 		// pause game
 	},
 
-	async списоксобытий1_Event7_Act1(runtime, localVars)
-	{
-		runtime.globalVars.coins += 100
-	},
-
 	async списоксобытий1_Event8_Act1(runtime, localVars)
 	{
 		// resume game
@@ -47,125 +42,125 @@ const scriptsInEvents = {
 		runtime.globalVars.playerName = usdk.adapter.playerName || ""
 	},
 
-	async списоксобытий1_Event30_Act1(runtime, localVars)
+	async списоксобытий1_Event28_Act1(runtime, localVars)
 	{
 		usdk.showInterstitial();
 	},
 
-	async списоксобытий1_Event31_Act1(runtime, localVars)
+	async списоксобытий1_Event29_Act1(runtime, localVars)
 	{
 		usdk.showRewarded();
 	},
 
-	async списоксобытий1_Event32_Act1(runtime, localVars)
+	async списоксобытий1_Event30_Act1(runtime, localVars)
 	{
 		usdk.showBanner();
 	},
 
-	async списоксобытий1_Event33_Act1(runtime, localVars)
+	async списоксобытий1_Event31_Act1(runtime, localVars)
 	{
 		usdk.hideBanner();
 	},
 
-	async списоксобытий1_Event34_Act1(runtime, localVars)
+	async списоксобытий1_Event32_Act1(runtime, localVars)
 	{
 		usdk.share({ text: "Score: " + runtime.globalVars.coins });
 	},
 
-	async списоксобытий1_Event35_Act1(runtime, localVars)
+	async списоксобытий1_Event33_Act1(runtime, localVars)
 	{
 		usdk.inviteFriends({ text: "Play with me!" });
 	},
 
-	async списоксобытий1_Event36_Act1(runtime, localVars)
+	async списоксобытий1_Event34_Act1(runtime, localVars)
 	{
 		usdk.authorizePlayer()
 		    .then(() => { runtime.globalVars.playerName = usdk.adapter.playerName || ""});
 	},
 
-	async списоксобытий1_Event37_Act1(runtime, localVars)
+	async списоксобытий1_Event35_Act1(runtime, localVars)
 	{
 		usdk.rateGame();
 	},
 
-	async списоксобытий1_Event39_Act2(runtime, localVars)
+	async списоксобытий1_Event37_Act2(runtime, localVars)
 	{
 		usdk.save("coins", runtime.globalVars.coins);
 		usdk.save("level", runtime.globalVars.level);
 		saveDict("World");
 	},
 
-	async списоксобытий1_Event41_Act2(runtime, localVars)
+	async списоксобытий1_Event39_Act2(runtime, localVars)
 	{
 		usdk.load("coins").then(v => { runtime.globalVars.coins = v })
 	},
 
-	async списоксобытий1_Event41_Act3(runtime, localVars)
+	async списоксобытий1_Event39_Act3(runtime, localVars)
 	{
 		usdk.load("level").then(v => { runtime.globalVars.level = v })
 	},
 
-	async списоксобытий1_Event41_Act5(runtime, localVars)
+	async списоксобытий1_Event39_Act5(runtime, localVars)
 	{
 		loadDict("World");
 	},
 
-	async списоксобытий1_Event42_Act1(runtime, localVars)
+	async списоксобытий1_Event40_Act1(runtime, localVars)
 	{
 		usdk.clipboardWrite("universalSDK " + location.href);
 	},
 
-	async списоксобытий1_Event43_Act1(runtime, localVars)
+	async списоксобытий1_Event41_Act1(runtime, localVars)
 	{
 		usdk.happyTime();
 	},
 
-	async списоксобытий1_Event44_Act1(runtime, localVars)
+	async списоксобытий1_Event42_Act1(runtime, localVars)
 	{
 		usdk.getPlayerName().then(n => { runtime.globalVars.playerName = n || "?";});
 	},
 
-	async списоксобытий1_Event45_Act1(runtime, localVars)
+	async списоксобытий1_Event43_Act1(runtime, localVars)
 	{
 		usdk.joinCommunity();
 	},
 
-	async списоксобытий1_Event46_Act1(runtime, localVars)
+	async списоксобытий1_Event44_Act1(runtime, localVars)
 	{
 		usdk.checkAdBlock();
 	},
 
-	async списоксобытий1_Event47_Act1(runtime, localVars)
+	async списоксобытий1_Event45_Act1(runtime, localVars)
 	{
 		usdk.addToHomeScreen();
 	},
 
-	async списоксобытий1_Event48_Act1(runtime, localVars)
+	async списоксобытий1_Event46_Act1(runtime, localVars)
 	{
 		usdk.addToFavorites();
 	},
 
-	async списоксобытий1_Event49_Act1(runtime, localVars)
+	async списоксобытий1_Event47_Act1(runtime, localVars)
 	{
 		usdk.gameplayStart(); 
 	},
 
-	async списоксобытий1_Event50_Act1(runtime, localVars)
+	async списоксобытий1_Event48_Act1(runtime, localVars)
 	{
 		usdk.gameplayStop(); 
 	},
 
-	async списоксобытий1_Event51_Act1(runtime, localVars)
+	async списоксобытий1_Event49_Act1(runtime, localVars)
 	{
 		usdk.leaderboard.show(10);
 	},
 
-	async списоксобытий1_Event52_Act1(runtime, localVars)
+	async списоксобытий1_Event50_Act1(runtime, localVars)
 	{
 		usdk.leaderboard.submitScore(runtime.globalVars.coins);
 	},
 
-	async списоксобытий1_Event53_Act1(runtime, localVars)
+	async списоксобытий1_Event51_Act1(runtime, localVars)
 	{
 		usdk.getLanguage();
 	}
