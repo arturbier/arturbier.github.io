c87526816762ecc28ec70edc6e749ad341fb134f
arturbier <78679486+arturbier@users.noreply.github.com>
2026-07-24 19:26:52 +0600

    -

--- nubimania/scripts/c3main.js
+++
@@ -1,6 +0,0 @@
-import "./../lodash.custom.min.js";
-import "./../tween.js";
-import "./c3runtime.js";
-import "./plugins/PlaygamaBridge/c3runtime/main.js";
-import "./objRefTable.js";
-import "./project/javaScriptInEvents.js";
