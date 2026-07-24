eb75f224f207cf34e805a829f99a6a53987ff51e
arturbier <78679486+arturbier@users.noreply.github.com>
2026-07-24 06:26:19 +0600

    -

---
+++ nubimania/scripts/c3main.js
@@ -0,0 +1,6 @@
+import "./../lodash.custom.min.js";
+import "./../tween.js";
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/usdk.js";
+import "./project/javaScriptInEvents.js";
